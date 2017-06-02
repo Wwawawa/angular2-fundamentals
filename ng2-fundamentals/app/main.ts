38c032f9561c8aafd47c91addd537f734198a3d1
Wwawawa <421299099@qq.com>
2017-06-02 17:16:59 +0800

    Update main.ts

--- ng2-fundamentals/app/main.ts
+++ ng2-fundamentals/app/main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
+//enable production mode
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule);
